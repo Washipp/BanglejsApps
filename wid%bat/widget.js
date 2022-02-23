b32194539b2b78f66e6fbfa36821dfb6ad5df1b5
Washipp <silas.meier@bluewin.ch>
2022-02-23 09:47:22 +0100

    Added files via upload

---
+++ wid%bat/widget.js
@@ -0,0 +1,29 @@
+(() => {
+  var normal = g.theme.dark ? "#fff" : "#000";
+  var background = g.theme.dark ? "#000" : "#fff";
+  var charging = "#0f0";
+  var red = "#f00";
+
+  function draw() {
+    var x = this.x, y = this.y;
+    g.reset();
+    l = E.getBattery();
+    l = Math.floor(Math.random() * 100);
+    var c = l <= 15 ? red : normal;
+    c = Bangle.isCharging() ? charging : c;
+    var s = l < 10 ? "0" + l : l;
+
+    g.setColor(background);
+    g.fillRect(x,y,x + 16, y + 6);
+    g.setColor(c);
+    g.setFont('6x8', 1.5);
+    g.drawString(s + "%", this.x, this.y);
+  }
+
+  WIDGETS["%bat"]={
+    area:"tr",
+    width: 16,
+    draw:draw
+  };
+  setInterval(()=>WIDGETS["%bat"].draw(), 60000);
+})();
